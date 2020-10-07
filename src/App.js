d7154b6e3dc0d27c030fcffcf932f1e0f7b7e90f
Jake Grella <jgrella13@gmail.com>
2020-10-07 16:39:55 -0500

    picture loads to page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import React from "react";
-import "./App.css";
+//React, hooks
+import React, { useState, useEffect } from 'react';
+//style
+import './App.css';
+//axios
+import axios from 'axios';
+//constants
+import { BASE_URL, API_KEY } from './constants/constants';
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
+	//state
+	// const [dates, setDates] = useState('');
+	const [date, setDate] = useState([]);
+
+	// console.log('first log', dates);
+	// effect hook
+	// useEffect(() => {
+	// 	const fetchPhoto = () => {
+	// 		axios
+	// 			.get(
+	// 				`${BASE_URL}?start_date=${dates}&end_date=${dates}&api_key=${API_KEY}`
+	// 			)
+	// 			.then((response) => {
+	// 				// setDates(response.data);
+	// 			})
+	// 			.catch((error) => {
+	// 				console.dir(error);
+	// 			});
+	// 	};
+	// 	fetchPhoto();
+	// });
+	// console.log('second log', dates);
+	useEffect(() => {
+		const fetchPhoto = () => {
+			axios
+				.get(`${BASE_URL}?api_key=${API_KEY}`)
+				.then((response) => {
+					setDate(response.data);
+				})
+				.catch((error) => {
+					console.dir(error);
+				});
+		};
+		console.log(fetchPhoto());
+		fetchPhoto();
+	}, []);
+
+	return (
+		<div className='App'>
+			<h1>{date.title}</h1>
+			<img src={date.url} alt='space' />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod';
+export const API_KEY = '80mKfQ2VHrN4NbFRGXoaP0pdgjeKAn5daIkCz92k';
